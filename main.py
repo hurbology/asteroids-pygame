8451ed1d526f08272c286a122a7f65b8aa68d597
Hurbology <owen@elderado.co.uk>
2026-06-29 17:05:57 +0100

    A: Added Game Loop to draw a black screen, return it's size and close the game/loop

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state 
 
 
 
 def main():
+    pygame.init()
+    pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    pygame.display.fill("black")
+    pygame.display.flip()
 
 if __name__ == "__main__":
     main()
